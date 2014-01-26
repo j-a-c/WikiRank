638942daf2d0733f24a0554f15482805b3e3ed61
Josh Campbell <runguy@ymail.com>
2014-01-25 19:33:00 -0500

    added Pagerank calculation and fixed count links.

--- PageRank/PageRank.java
+++ PageRank/PageRank.java
@@ -1,5 +1,7 @@
 package PageRank;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
 import java.io.IOException;
 import java.lang.StringBuilder;
 import java.util.Iterator;
@@ -7,6 +9,7 @@ import java.util.regex.*;
 import java.util.HashSet;
 import java.util.Set;
 
+import org.apache.hadoop.fs.FileSystem;
 import org.apache.hadoop.fs.Path;
 import org.apache.hadoop.conf.*;
 import org.apache.hadoop.io.*;
@@ -32,17 +35,35 @@ import PageRank.XmlInputFormat;
  */
 public class PageRank 
 {
+    // The number of iterations to run the PageRank algorithm.
+    private static final int NUM_PAGERANK_ITERS = 2;
+
+    // The number of pages total in the PageRank matrix.
+    // Needs to be set before calculating the PageRank.
+    private static int NUM_PAGES_TOTAL;
+    // This is the start index of NUM_PAGES_TOTAL in the file.
+    // See CountReducer for the output format.
+    private static final int NUM_PAGES_TOTAL_START = 2; 
+
+    // Residual probablility and 1 - residual probablility.
+    private static final double DELTA = 0.85;
+    private static final double ONEMINUSDELTA = 1.0 - DELTA;
+
+    // Whitespace tokenizer.
+    private static final String WHITESPACE = "\\s+";
 
     // Bucket that we will be operating in.
-    String bucketName;
+    private String bucketName;
     // Input location for the Wikipedia XML dump.
-    String XMLinputLocation;
+    private String XMLinputLocation;
     // Output for the parsed Wikipedia XML dump.
-    String XMLoutputLocation;
+    private String XMLoutputLocation;
     // Input location for the job that counts the number of pages.
-    String CountInputLocation;
+    private String CountInputLocation;
     // Output location for the job that counts the number of pages.
-    String CountOutputLocation;
+    private String CountOutputLocation;
+    // Temporary matrix output location.
+    private String tempMatrixOutput;
 
     /**
      * Constructor for a PageRank job.
@@ -51,12 +72,12 @@ public class PageRank
      */
     public PageRank(String bucketName)
     {
-        // TODO Uncomment for submission.
+        // TODO Uncomment for submission. And test.
         // this.bucketName = "s3n://" + bucketName;
         // this.XMLinputLocation = "s3://spring-2014-ds/data/enwiki-latest-pages-articles.xml";
         
-        // TODO Delete before submission!
-        this.bucketName = "/" + bucketName;
+        // TODO Delete the two below here before submission!
+        this.bucketName = "hdfs://localhost:54310/" + bucketName;
         this.XMLinputLocation = "/wiki-pages.xml";
 
         // Keep the file paths below.
@@ -64,7 +85,9 @@ public class PageRank
         this.XMLoutputLocation = this.bucketName + "/results/PageRank.inlink.out";
         // Input and output location for the count job.
         this.CountInputLocation = this.XMLoutputLocation;
-        this.CountOutputLocation = this.bucketName + "/results/PageRank.n.out";
+        this.CountOutputLocation = this.bucketName + "/results/PageRank.n.out/part";
+        // Temporary matrix input and output locations.
+        this.tempMatrixOutput = this.bucketName + "/tmp/matrixOut";
     }
 
     /**
@@ -179,57 +202,65 @@ public class PageRank
      * Maps each page to <1,PageTitle>.
      * This mapper will be receiving the output produced from parseXML();
      * The input will be of the form "pageTitle link1 link2...".
+     * We split the input by whitespace and output each token.
      */
     public static class CountMapper extends MapReduceBase implements 
-        Mapper<LongWritable, Text, IntWritable, IntWritable> 
+        Mapper<LongWritable, Text, IntWritable, Text> 
     {
         // We will use this to group all the pages.
         private final static IntWritable one = new IntWritable(1);
+
+        private Text outVal = new Text();
 
         public CountMapper(){}
 
         // map(key, value, OutputCollector<KeyOut,ValueOut>)
         public void map(LongWritable keyIn, Text pageStats, 
-                OutputCollector<IntWritable, IntWritable> output, 
+                OutputCollector<IntWritable, Text> output, 
                 Reporter reporter) throws IOException 
         {
-            // The value will be the page title.
-            //Text outVal = new Text(pageStats.toString().split("\\s+")[0]);
-            output.collect(one, one);
+            for (String page : pageStats.toString().split(WHITESPACE))
+            {
+                outVal.set(page);
+                output.collect(one, outVal);
+            }
         }
     }
 
    /**
-     * Counts the number of values (pages) this mapper has seen.
+     * Only outputs the unique links from the CountMapper.
      * Receives the output of CountMapper.
-     * Now, there shuffle/sort step will be smaller and the reducer can just
-     * add a couple values.
+     * Now, there shuffle/sort step will be smaller. 
      *
      * Reducer<KeyIn, ValueIn, KeyOut, ValueOut>
      */
     public static class CountCombiner extends MapReduceBase implements
-        Reducer<IntWritable, IntWritable, IntWritable, IntWritable> 
+        Reducer<IntWritable, Text, IntWritable, Text> 
     {
         // We will use this to group all the pages at the reducer.
         private final static IntWritable one = new IntWritable(1);
+
+        private Text outVal = new Text();
 
         public CountCombiner(){}
 
         // reduce(KeyIn key, Iterator<ValueIn> values, 
         // OutputCollector<KeyOut,ValueOut> output, Reporter reporter) 
-        public void reduce(IntWritable key, Iterator<IntWritable> values, 
-                OutputCollector<IntWritable, IntWritable> output, 
+        public void reduce(IntWritable key, Iterator<Text> values, 
+                OutputCollector<IntWritable, Text> output, 
                 Reporter reporter) throws IOException 
         {
-            // Count all the links this mapper has seen.
-            int count = 0;
+            HashSet<String> uniques = new HashSet<String>();
             while (values.hasNext())
             {
-                count++;
-                values.next();
+                uniques.add(values.next().toString());
             }
-
-            output.collect(one, new IntWritable(count)); 
+    
+            for (String unique : uniques)
+            {
+                outVal.set(unique);
+                output.collect(one, outVal); 
+            }
         }
     }
 
@@ -244,26 +275,27 @@ public class PageRank
      * Reducer<KeyIn, ValueIn, KeyOut, ValueOut>
      */
     public static class CountReducer extends MapReduceBase implements
-        Reducer<IntWritable, IntWritable, Text, Text> 
+        Reducer<IntWritable, Text, Text, Text> 
     {
+        private Text outKey = new Text();
+        private final Text outVal = new Text("");
+
         public CountReducer(){}
 
         // reduce(KeyIn key, Iterator<ValueIn> values, 
         // OutputCollector<KeyOut,ValueOut> output, Reporter reporter) 
-        public void reduce(IntWritable key, Iterator<IntWritable> values, 
+        public void reduce(IntWritable key, Iterator<Text> values, 
                 OutputCollector<Text, Text> output, 
                 Reporter reporter) throws IOException 
         {
-            // Count all the links.
-            int count = 0;
+
+            HashSet<String> uniques = new HashSet<String>();
             while (values.hasNext())
             {
-                count += values.next().get();
+                uniques.add(values.next().toString());
             }
 
-            Text outKey = new Text("N="+count);
-            Text outVal = new Text("");
-
+            outKey.set("N="+uniques.size());
             output.collect(outKey, outVal); 
         }
     }
@@ -289,9 +321,6 @@ public class PageRank
         // Combiner for the mapper.
         conf.setCombinerClass(CountCombiner.class);
 
-        // Let us split the file into small pieces.
-        //conf.setNumMapTasks(10);
-
         // We will only have one reducer so we can count all the pages.
         conf.setNumReduceTasks(1);
         conf.setReducerClass(CountReducer.class);
@@ -306,6 +335,218 @@ public class PageRank
         conf.setOutputFormat(TextOutputFormat.class);
 
         JobClient.runJob(conf);
+    }
+
+    /**
+     * For each "page OutgoingLinksText" produces the following output:
+     *      <outLink, contribution>
+     *          This output can be used to calculate the PageRank of outLink in
+     *          the Reducer.
+     *      <in, '! origLinks'>.
+     *          This output can be used to calculate the new PageRank matrix
+     *          (once the PageRank for the in page is calculated). The first
+     *          character of input value to the reducer will be '!' so the
+     *          reducer can distinguish between the two output formats.
+     *
+     *  The first iteration will not have a PageRank associated with it, so we
+     *  need to distinguish it using firstIteration. We will assign PageRank =
+     *  1.0 for all pages. The first iteration receives inputs as follows:
+     *      pageTitle outlink1 outlink2...
+     */
+    public static class PageRankMapper extends MapReduceBase implements 
+        Mapper<LongWritable, Text, Text, Text> 
+    {
+        public static boolean firstIteration = true;
+
+        private Text key = new Text();
+        private Text value = new Text();
+
+        public PageRankMapper(){}
+
+        // map(key, value, OutputCollector<KeyOut,ValueOut>)
+        public void map(LongWritable keyIn, Text pageStats, 
+                OutputCollector<Text, Text> output, 
+                Reporter reporter) throws IOException 
+        {
+            String[] tokens = pageStats.toString().split(WHITESPACE);
+
+            // Will be used for the second type of output.
+            StringBuilder builder = new StringBuilder();
+            builder.append('!');
+            builder.append(' ');
+
+            // Output the first type of output.
+            if (firstIteration)
+            {
+                // Calculate the PageRank contribution from the parent.
+                int numLinks = tokens.length - 1;
+                double parentPR = 1.0;
+                String contribution = String.valueOf(parentPR / numLinks);
+
+                for (int index = 1; index < tokens.length; index++)
+                {
+                    key.set(tokens[index]);
+                    value.set(contribution);
+                    output.collect(key, value);
+
+                    builder.append(tokens[index]);
+                    builder.append(' ');
+                }
+
+                // Output the second type of output
+                key.set(tokens[0]);     
+                value.set(builder.toString());
+                output.collect(key, value);
+
+            }
+            else // We are receiving an input from the PageRank matrix.
+            {
+                int numLinks = tokens.length - 2;
+                // The first token will be the parents PageRank.
+                double parentPR = Double.parseDouble(tokens[1]);
+                String contribution = String.valueOf(parentPR / numLinks);
+
+                for (int index = 2; index < tokens.length; index++)
+                {
+                    key.set(tokens[index]);
+                    value.set(contribution);
+                    output.collect(key, value);
+
+                    builder.append(tokens[index]);
+                    builder.append(' ');
+                }
+
+                // Output the second type of output
+                key.set(tokens[0]);     
+                value.set(builder.toString());
+                output.collect(key, value);
+            }
+
+        }
+    }
+
+    /**
+     * Computes the final PageRank. The PageRankMapper will send two types of
+     * output; their uses are shown below:
+     *      <outLink, contribution>
+     *          Will be used to compute the PageRank. These are partial
+     *          PageRanks computed from a parent page.
+     *      <in, '! origLinks'>.
+     *          Replace the '!' with the complete PageRank.
+     */
+    public static class PageRankReducer extends MapReduceBase implements
+        Reducer<Text, Text, Text, Text> 
+    {
+        public PageRankReducer(){}
+
+        private Text outKey = new Text();
+        private final Text outVal = new Text("");
+
+        // reduce(KeyIn key, Iterator<ValueIn> values, 
+        // OutputCollector<KeyOut,ValueOut> output, Reporter reporter) 
+        public void reduce(Text key, Iterator<Text> values, 
+                OutputCollector<Text, Text> output, 
+                Reporter reporter) throws IOException 
+        {
+            // The matrix row. 
+            String matrixString = null;
+
+            // The final PageRank (not fully computed at this point).
+            double finalPR = (ONEMINUSDELTA) / NUM_PAGES_TOTAL;
+    
+            double tempPR = 0.0;
+            while(values.hasNext())
+            {
+                String value = values.next().toString();
+
+                if (value.startsWith("!"))
+                    matrixString = value;
+                else
+                    tempPR += Double.parseDouble(value);
+            }
+            
+            finalPR = finalPR + (DELTA * tempPR);
+
+            if (matrixString == null)
+            {
+                matrixString = key + " " + finalPR;
+            }
+            else
+            {
+                matrixString = matrixString.replace("!", String.valueOf(finalPR));
+            }
+
+            outKey.set(matrixString);
+            output.collect(outKey, outVal);
+        }
+    }
+
+    /**
+     * Calculates the PageRank of the Wikipedia pages.
+     */
+    public void calculatePageRank() throws IOException
+    {
+        // Set the total number of links.
+        try
+        {
+            Path pt = new Path(this.CountOutputLocation + "/part-00000");
+            FileSystem fs = FileSystem.get(new Configuration());
+            BufferedReader br = new BufferedReader(new InputStreamReader(fs.open(pt)));
+            String line = br.readLine();
+            line = line.substring(NUM_PAGES_TOTAL_START);
+            line = line.split(WHITESPACE)[0];
+            this.NUM_PAGES_TOTAL = Integer.parseInt(line);
+        }
+        catch(Exception e)
+        {
+            System.out.println("Error: ");
+            System.out.println(e);
+            return;
+        }
+
+        // Run the PageRank calulation NUM_PAGERANK_ITERS times.
+        // The index starts from 1 just for naming purposes.
+        for (int i = 1; i <= NUM_PAGERANK_ITERS; i++)
+        {
+            // Configuration for this job.
+            JobConf conf = new JobConf(PageRank.class);
+            conf.setJobName("PageRankIteration" + i);
+
+            if (i == 1)
+            {
+                // Input location. On the first iteration, we will read the output
+                // from the XML mapper.
+                FileInputFormat.setInputPaths(conf, new Path(this.XMLoutputLocation));
+            }
+            else
+            {
+                // For the other iterations, we will read from the matrix file.
+                FileInputFormat.setInputPaths(conf, new Path(this.tempMatrixOutput + (i-1)));
+            }
+
+            // Input type.
+            conf.setInputFormat(TextInputFormat.class);
+
+            // Mapper class.
+            conf.setMapperClass(PageRankMapper.class);
+
+            // Reducer class.
+            conf.setReducerClass(PageRankReducer.class);
+     
+            // Output configuration.
+            FileOutputFormat.setOutputPath(conf,  new Path(this.tempMatrixOutput + i));
+            conf.setOutputKeyClass(Text.class);
+            conf.setOutputValueClass(Text.class);
+
+            // Output type.
+            conf.setOutputFormat(TextOutputFormat.class);
+
+            JobClient.runJob(conf);
+            
+            // Set the first iteration marker to false.
+            PageRankMapper.firstIteration = false;
+        }
+
     }
 
     /**
@@ -327,12 +568,13 @@ public class PageRank
         // Parse the XML to map input links to output links.
         pagerank.parseXML();
 
-        // TODO Count number of pages.
+        // Count number of pages.
         pagerank.countPages();
 
-        // TODO Compute PageRank.
-        // TODO Sort PageRank.
+        // Compute PageRank.
+        pagerank.calculatePageRank();
 
+        // TODO Sort PageRank.
 
     }
 }
